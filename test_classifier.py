7f8986cbf2090fa9a983bb17b5c744b40f749ec7
Fritz1414213562 <fritz1414213562@gmail.com>
2021-06-29 16:39:58 +0900

    change neuron number and batch size

--- test_classifier.py
+++ test_classifier.py
@@ -5,32 +5,12 @@ from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.layers import Dense, Activation, Masking
 from tensorflow.keras.layers import LSTM
+from iomanip import read_dataset
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
 import math
 import subprocess
-
-
-
-def read_dataset(filename):
-	import h5py
-	import numpy as np
-
-	with h5py.File(filename, 'r') as ifs:
-
-		data_shape = tuple(ifs["DataSize/shape"])
-		dataset = np.zeros(data_shape, dtype=np.int16)
-		labels = np.zeros((data_shape[0],), dtype=np.int8)
-
-		for idx in range(data_shape[0]):
-			if idx % 100000 == 0:
-				print(idx, file = sys.stderr)
-			key_path = "DataSet/Instance" + str(idx) + "/"
-			dataset[idx, :] = np.array(ifs[key_path + "squiggle"], dtype=np.int16)
-			labels[idx] = np.array(ifs[key_path + "label"], dtype=np.int8)[0]
-	
-	return dataset, labels
 
 
 def main():
@@ -40,21 +20,23 @@ def main():
 
 	data = list()
 
-	dataset, labels = read_dataset("dataset.ds5")
+	dataset, labels = read_dataset("dataset.ds5", density = True)
 	instance_num = dataset.shape[0]
 	
-	n_hidden = 500
+	n_hidden = 100
+	in_neurons = 1
 	out_neurons = 1
 	batch_size = 1024
 	
 	model = Sequential()
+	model.add(Masking(mask_value = 0, input_shape = (1, dataset.shape[1])))
 	model.add(Dense(n_hidden))
 	model.add(Activation("relu"))
 	model.add(Dense(out_neurons))
 	model.add(Activation("sigmoid"))
 	optimizer = Adam(learning_rate = 0.001)
 	model.compile(loss = "mean_squared_error", optimizer = optimizer)
-#	print(model.summary())
+	# print(model.summary())
 
 	#sys.exit()
 	early_stopping = EarlyStopping(monitor = "val_loss", mode = "auto", patience = 20)
