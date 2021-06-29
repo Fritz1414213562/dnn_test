5e9d247e2df8311ef53c5673e5755ea9a78f208a
Fritz1414213562 <fritz1414213562@gmail.com>
2021-06-29 16:41:10 +0900

    add read_dataset funcion

--- iomanip/__init__.py
+++ iomanip/__init__.py
@@ -5,3 +5,4 @@ from .write_fastl import dump_fastl
 from .read_files_in_dir import *
 from .read_aligned_id import read_id_from_paf
 from .read_fastl import read_fastl
+from .read_dataset import read_dataset
