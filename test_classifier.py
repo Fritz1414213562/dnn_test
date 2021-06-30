da1125a796a20c431a92ac7e679a65717b0c920f
Fritz1414213562 <fritz1414213562@gmail.com>
2021-06-30 10:19:31 +0900

    fix grammers for tensorflow v-1.12

--- test_classifier.py
+++ test_classifier.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras import layers, preprocessing
+from tensorflow.keras import models, layers, preprocessing
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import EarlyStopping
@@ -7,7 +7,6 @@ from tensorflow.keras.layers import Dense, Activation, Masking
 from tensorflow.keras.layers import LSTM
 from iomanip import read_dataset
 import numpy as np
-import matplotlib.pyplot as plt
 import sys
 import math
 import subprocess
@@ -15,26 +14,30 @@ import subprocess
 
 def main():
 
-	tf.random.set_seed(199803132)
+#	tf.random.set_seed(199803132)
+	tf.random.set_random_seed(199803132)
 	np.random.seed(271828189)
 
 	data = list()
 
-	dataset, labels = read_dataset("dataset.ds5", density = True)
+	dataset, labels = read_dataset("input_data/dataset.ds5", density = True)
 	instance_num = dataset.shape[0]
 	
-	n_hidden = 100
+	n_hidden = 250
 	in_neurons = 1
 	out_neurons = 1
 	batch_size = 1024
 	
 	model = Sequential()
-	model.add(Masking(mask_value = 0, input_shape = (1, dataset.shape[1])))
+	model.add(Masking(mask_value = 0))
+	model.add(Dense(n_hidden))
+	model.add(Activation("relu"))
 	model.add(Dense(n_hidden))
 	model.add(Activation("relu"))
 	model.add(Dense(out_neurons))
 	model.add(Activation("sigmoid"))
-	optimizer = Adam(learning_rate = 0.001)
+	#optimizer = Adam(learning_rate = 0.001)
+	optimizer = Adam()
 	model.compile(loss = "mean_squared_error", optimizer = optimizer)
 	# print(model.summary())
 
@@ -42,7 +45,10 @@ def main():
 	early_stopping = EarlyStopping(monitor = "val_loss", mode = "auto", patience = 20)
 	#model.fit(small_data, small_target, batch_size = batch_size, epochs = 100, validation_split = 0.1, callbacks = [early_stopping])
 	model.fit(dataset, labels, batch_size = batch_size, epochs = 100, validation_split = 0.1, callbacks = [early_stopping])
-	model.save("test_model")
+	#model.save("test_model")
+	#models.save_model(model, filepath = "test_model")
+	#models.save_model(model, filepath = "test_model_h2_n100")
+	models.save_model(model, filepath = "test_model_h2_n250")
 	
 	## using train data
 	validate_size = 1000
