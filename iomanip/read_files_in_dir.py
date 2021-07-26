b0da37bf33e18ba4c3e226f3530f1fca223dc655
Fritz1414213562 <fritz1414213562@gmail.com>
2021-07-26 13:04:06 +0900

    Fix the bug when there is just one input path

--- iomanip/read_files_in_dir.py
+++ iomanip/read_files_in_dir.py
@@ -13,9 +13,20 @@ def filesindir(dirpath, suffix):
 
 def filesindirs(dirs, suffix):
 
-	retval = list()
+	import sys
 
-	for dir in dirs:
+	retval = list()
+	cand_dirs = ...
+
+	if isinstance(dirs, str):
+		cand_dirs = [dirs]
+	elif isinstance(dirs, list):
+		cand_dirs = dirs
+	else:
+		print("Error: Internal Error, The type of file directories should be list or str", file = sys.stderr)
+		sys.exit()
+
+	for dir in cand_dirs:
 		retval[len(retval):len(retval)] = filesindir(dir, suffix)
 	
 
